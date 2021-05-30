b43154a2423628d86e9760ac140a61c711277cfd
harrisonblackburn <blackbhr@yahoo.com>
2021-05-29 23:38:50 -0500

    added some styling and working onpages/comps

---
+++ pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+import API from "../utils/API";
+
+class Employee extends Component {
+    state = {
+      name: "",
+      gender: "",
+      location: [], 
+      id: "", 
+      email: "",
+      picture: "",
+      
+    };
+}
+
